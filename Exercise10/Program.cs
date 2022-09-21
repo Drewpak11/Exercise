5cd04d92e45d482f4a00690186edc9d3739c79b7
Andrei <aleinik88.aa@gmail.com>
2022-09-21 16:33:15 +0300

    Exercise10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число: ");
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+int x = num / 100;
+int y = (num / 10) % 10;
+int z = num % 10;
+if(x < z || x > z) Console.WriteLine(y);
