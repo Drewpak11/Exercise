fe0218b5e4bd57ec198fd1e72a5844b2ae78d9ce
Andrei <aleinik88.aa@gmail.com>
2022-09-27 14:53:51 +0300

    Exercise23

---
+++ Exercise23/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+int y = 1;
+
+
+while(x <= num)
+{
+    y = x * x * x;
+    x++;
+    Console.WriteLine(y);
+}
