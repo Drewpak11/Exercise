0a79be1bc9ab8e39a47dcf724c0e1ffe32ee8617
Andrei <aleinik88.aa@gmail.com>
2022-09-27 16:03:11 +0300

    Exercise21

---
+++ Exercise21/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+Console.WriteLine("Введите координату x для первой точки: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для первой точки: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для первой точки: ");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату x для второй точки: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для второй точки: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для второй точки: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya) + (zb - za) * (zb - za));
+Console.WriteLine($"Расстояние между точками {Math.Round(d, 2)}");
