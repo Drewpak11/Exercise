653803b8c3ca9f09397b625d5c479b18593682a0
Andrei <aleinik88.aa@gmail.com>
2022-09-28 14:27:18 +0300

    Exercise29

---
+++ Exercise29/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = new int [8];
+int i = 0;
+
+while(i < array.Length) //Можно было и через for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write($"{array[i]}, ");
+    i++;
+}
