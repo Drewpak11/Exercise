25f2386a5d1ddd330a7e2f56e2394b7116b76bd6
Andrei <aleinik88.aa@gmail.com>
2022-10-12 16:30:41 +0300

    Exercise47

---
+++ Exercise47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+
+void FillArr(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+
+    }
+}
+
+
+void PrintArr(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j] / 10} |");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+double[,] array = new double[3, 3];
+FillArr(array);
+PrintArr(array);
